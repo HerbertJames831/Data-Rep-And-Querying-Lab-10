e5bbb67c59e49b48838ce588804e968250799fda
HerbertJames831 <HerbertJames831@gmail.com>
2022-10-23 22:18:51 +0100

    Lab 4 first commit to cloned lab3 repo

--- src/components/create.js
+++ src/components/create.js
@@ -1,8 +1,55 @@
 import React from 'react';
 
-
 export class Create extends React.Component {
+    constructor() {
+        super();
+        //In React, the bind() method is an inbuilt method that is utilized to pass the data as an argument to a class based component's function
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.onChangeBookTitle = this.onChangeBookTitle.bind(this);
+        this.onChangeBookCover = this.onChangeBookCover.bind(this);
+        this.onChangeBookAuthor = this.onChangeBookAuthor.bind(this);
+        this.state = {
 
+            title: '',
+            cover: '',
+            author: ''
+        }
+    }
+    //handleSubmit is responsible for retrieving the state's current value and adding it to the webhooks array 
+    handleSubmit(e) {
+
+        //The preventDefault() method helps to cancel the event if it is cancelable; this means the default action which belongs to the event will not happen
+        e.preventDefault();
+        console.log(`${this.state.title},
+  ${this.state.cover},
+  ${this.state.author}`);
+
+    }
+    //onChange is an event handler in React that gets triggered whenever the input field for book title is changed
+    onChangeBookTitle(e) {
+        //setState() helps to enqueue changes to the component state and informs React that it is essential this component and its children must be re-rendered with the updated state
+        this.setState({
+            //Event Target Value is beneficial for getting the value of the book title element
+            title: e.target.value
+        })
+    }
+    //onChange is an event handler in React that gets triggered whenever the input field for book cover is changed
+    onChangeBookCover(e) {
+        //setState() aids to enqueue changes to the component state and informs React that it is vital this component and its children must be re-rendered with the updated state
+        this.setState({
+            //Event Target Value is useful for getting the value of the book cover element
+            cover: e.target.value
+        })
+    }
+    //onChange is an event handler in React that gets triggered whenever the input field for book author is changed
+    onChangeBookAuthor(e) {
+        //setState() assists in enqueuing changes to the component state and informs React that it is crucial this component and its children must be re-rendered with the updated state
+        this.setState({
+            //Event Target Value is beneficial for getting the value of the book author element
+            author: e.target.value
+        })
+    }
+    //Helps display specific HTML code inside the specified HTML element
     render() {
 
         return (
@@ -10,12 +57,40 @@ export class Create extends React.Component {
             <div>
 
                 <h3>Hello from Create Component</h3>
+                {/* Handling new events from the button click and logging the information that is submitted by the user in the form to the console */}
+                <form onSubmit={this.handleSubmit}>
+                    {/* Book Title Input Control */}
+                    <div className="form-group">
+                        <label>Add Book Title: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.title}
+                            onChange={this.onChangeBookTitle} />
+                    </div>
 
+                    {/* Book Cover Input Control */}
+                    <div className="form-group">
+                        <label>Add Book Cover: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.cover}
+                            onChange={this.onChangeBookCover} />
+                    </div>
+
+                    {/* Book Author Input Control */}
+                    <div className="form-group">
+                        <label>Add Book Author: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.author}
+                            onChange={this.onChangeBookAuthor} />
+
+                        <input type="submit" value="Add Book" />
+
+                    </div>
+
+                </form>
             </div>
-
-
         )
-
     }
-
 }
--- src/components/read.js
+++ src/components/read.js
@@ -1,49 +1,38 @@
 import React from 'react';
 import { Books } from './books';
+import axios from "axios";
 
 export class Read extends React.Component {
 
+    //When the component is already placed in the DOM(Document Object Model), the componentDidMount() method permits the React code to be executed
+    componentDidMount() {
+
+        //Axios is a promise-based HTTP library that allows developers to make requests to a third-party server or their own to fetch data
+        //Axios provides several different ways of making requests such as GET, POST, PUT/PATCH and DELETE
+        axios.get('https://jsonblob.com/api/jsonblob/1027219693823606784')
+            .then((response) => {
+                //setState() assists in enqueuing changes to the component state and informs React that it is essential this component and its children must be re-rendered with the updated state
+                this.setState({
+
+                    books: response.data
+
+                })
+
+            })
+            .catch((error) => {
+
+                console.log(error);
+
+            });
+
+    }
     //Utilized to carry data about the component 
     state = {
-        //List of Information about Books*(JSON code)
-        books: [
-            {
-                "title": "Learn Git in a Month of Lunches",
-                "isbn": "1617292419",
-                "pageCount": 0,
-                "thumbnailUrl": "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/umali.jpg",
-                "status": "MEAP",
-                "authors": ["Rick Umali"],
-                "categories": []
-            },
-            {
-                "title": "MongoDB in Action, Second Edition",
-                "isbn": "1617291609",
-                "pageCount": 0,
-                "thumbnailUrl": "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/banker2.jpg",
-                "status": "MEAP",
-                "authors": [
-                    "Kyle Banker",
-                    "Peter Bakkum",
-                    "Tim Hawkins",
-                    "Shaun Verch",
-                    "Douglas Garrett"
-                ],
-                "categories": []
-            },
-            {
-                "title": "Getting MEAN with Mongo, Express, Angular, and Node",
-                "isbn": "1617292036",
-                "pageCount": 0,
-                "thumbnailUrl": "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/sholmes.jpg",
-                "status": "MEAP",
-                "authors": ["Simon Holmes"],
-                "categories": []
-            }
-        ]
+        //List of Information about Books (JSON code)
+        books: []
     }
 
-    //Helps displays specific HTML code inside the specified HTML element
+    //Helps display specific HTML code inside the specified HTML element
     render() {
 
         return (
